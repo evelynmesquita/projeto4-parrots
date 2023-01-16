dc9f992b822f93fa32d07e55fab8579565341dbb
evelyn <evelynbandeiramesquita@gmail.com>
2023-01-16 12:50:15 -0300

    adicionando array de imagens

---
+++ script.js
@@ -0,0 +1,10 @@
+/*****ARRAY IMAGENS****/
+const cartasImg = 
+    ["media/photos/bobrossparrot.gif",
+    "media/photos/explodyparrot.gif",
+    "media/photos//fiestaparrot.gif",
+    "media/photos/metalparrot.gif", 
+    "media/photos/revertitparrot.gif",
+    "media/photos/tripletsparrot.gif",
+    "media/photos//unicornparrot.gif"]
+
